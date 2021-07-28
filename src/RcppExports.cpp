ad852875749d7db034929c98cec8ffee5f6283cf
Lijun Wang <szcfweiya@gmail.com>
2021-07-28 09:50:05 +0000

    version 1.2

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // frlr1
 Rcpp::List frlr1(SEXP R_X, SEXP R_Y, SEXP R_COV);
 RcppExport SEXP _fRLR_frlr1(SEXP R_XSEXP, SEXP R_YSEXP, SEXP R_COVSEXP) {
@@ -15,19 +20,6 @@ BEGIN_RCPP
     Rcpp::traits::input_parameter< SEXP >::type R_Y(R_YSEXP);
     Rcpp::traits::input_parameter< SEXP >::type R_COV(R_COVSEXP);
     rcpp_result_gen = Rcpp::wrap(frlr1(R_X, R_Y, R_COV));
-    return rcpp_result_gen;
-END_RCPP
-}
-// frlr
-Rcpp::List frlr(SEXP R_X, SEXP R_Y, SEXP R_COV);
-RcppExport SEXP _fRLR_frlr(SEXP R_XSEXP, SEXP R_YSEXP, SEXP R_COVSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< SEXP >::type R_X(R_XSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type R_Y(R_YSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type R_COV(R_COVSEXP);
-    rcpp_result_gen = Rcpp::wrap(frlr(R_X, R_Y, R_COV));
     return rcpp_result_gen;
 END_RCPP
 }
@@ -49,7 +41,6 @@ END_RCPP
 
 static const R_CallMethodDef CallEntries[] = {
     {"_fRLR_frlr1", (DL_FUNC) &_fRLR_frlr1, 3},
-    {"_fRLR_frlr", (DL_FUNC) &_fRLR_frlr, 3},
     {"_fRLR_frlr2", (DL_FUNC) &_fRLR_frlr2, 5},
     {NULL, NULL, 0}
 };
